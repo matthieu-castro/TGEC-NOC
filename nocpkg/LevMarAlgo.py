Correcting bugs in implementation parallel multiprocessing calculation when using with seismic constraints

nocpkg/LevMarAlgo.py
@@ -138,7 +138,7 @@ class LevMar:
             old_param = np.array([p.value for p in self.new_param])
 
             for i, p in enumerate(self.parameters):
-                self.new_param[i].value = max(min(p.value + np.sign(dparam[i])*min(np.abs(dparam[i]),
+                self.new_param[i].value = np.max(np.min(p.value + np.sign(dparam[i])*np.min(np.abs(dparam[i]),
                                                   np.abs(p.value*p.rate/100.0)), p.bounds[1]), p.bounds[0])
 
             text += "\nNew parameters:\n"
@@ -426,6 +426,7 @@ class LevMar:
         error = False
 
         # Calculation of the central model and the shifted models
+        # With multiprocessing calculation
         if self.multiproc:
             self.__add_process(0, self.func_shell, parameters, levmar_args)
             self.nproc += 1
@@ -437,33 +438,44 @@ class LevMar:
             for proc in self.processes:
                 proc.join()
 
-            shift_model = np.ones((self.ny, self.nproc))
-            steps = np.zeros(self.nproc-1)
+            shift_model = np.ones((self.ny, len(self.parameters)))
+            steps = np.zeros(len(self.parameters))
+            results = []
 
             if any([proc.exitcode for proc in self.processes]):
                 raise NOCError("LevMar.levmar_step failed")
 
-            # print(f"Queue size before reading results = {self.queue.qsize()}")
-
+            # Get results
+            index = 0
+            model = []
+            step = 0
             for _ in range(self.nproc):
                 try:
                     index, model, step, error = self.queue.get_nowait()
                 except queue.Empty:
                     print("Queue is empty")
-                # print(f"Queue size after getting model {self.proc_index[index+1]} = {self.queue.qsize()}")
                 if error:
                     return True, [], []
                 if index == -1:
                     center_model = model
+                results.append((index, model, step, error))
+
+            # We add the shifted models with seismic parameter and reorder the outputs
+            for i, p in enumerate(self.parameters):
+                if not p.seismic:
+                    index, model, step, error = results[i]
+                    if index != -1 and self.proc_index[index+1] == p.name:
+                        shift = model
+                        steps[i] = step
+                        self.__reorder_outputs(shift, shift_model[:, i])
                 else:
-                    for i, p in enumerate(self.parameters):
-                        if self.proc_index[index+1] == p.name:
-                            shift = model
-                            steps[i] = step
-                            self.__reorder_outputs(shift, shift_model[:, i])
+                    func_args_tmp = self.__get_func_args_tmp(levmar_args, p.name)
+                    (shift, steps[i], error) = self.func_deriv(self.func, parameters, func_args_tmp, i)
+                    if error:
+                        return True, [], []
+                    self.__reorder_outputs(shift, shift_model[:, i])
 
-            # print(f"Queue size after getting results = {self.queue.qsize()}")
-
+        # Without multiprocessing calculation
         else:
             func_args_tmp = levmar_args[0]['center'], levmar_args[0]['center'], levmar_args[1]
             center_model[:], error = self.func(parameters, func_args_tmp, -1)